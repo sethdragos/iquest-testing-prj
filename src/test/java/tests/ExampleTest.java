10256bf3fcdfb6f63bcb9abe6b1fc9cb6da02451
Dragos Serghie <dragos.serghie@ft.com>
2013-11-04 12:08:12 +0200

    Test class and properties file examples

---
+++ src/test/java/tests/ExampleTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dragos.serghie
+ * Date: 11/4/13
+ * Time: 12:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExampleTest {
+
+    // empty class
+}
